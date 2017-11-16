801934755420efb32757cdf88b98c7c76d01a9fa
dllo <dllo@liyandeMac-mini.local>
2017-11-16 17:29:08 +0800

    版本升级

--- webpack-vue-yintai/src/router/index.js
+++ webpack-vue-yintai/src/router/index.js
@@ -1,12 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Cube from 'cube-ui'
 
-Vue.use(Cube)
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
