dd4e07e54e4547ab05df9c401e14e1965bcb5d3a
dllo <dllo@liyandeMac-mini.local>
2017-11-16 20:18:32 +0800

    无敌版

--- webpack-vue-yintai/src/router/index.js
+++ webpack-vue-yintai/src/router/index.js
@@ -1,16 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import DuanRoutes from '@/router/duanyiwen'
+import LiuRoutes from '@/router/liuxin'
+import LuRoutes from '@/router/luhanran'
+import LiRoutes from '@/router/liyan'
 
 Vue.use(Router)
 
+let newRoutes = DuanRoutes.concat(LuRoutes).concat(LiuRoutes).concat(LiRoutes)
 export default new Router({
   mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes: newRoutes
 })
