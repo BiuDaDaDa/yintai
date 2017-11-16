ec7bf53e13ff929d9969ee3aa4ac9cbb2dfe3938
dllo <dllo@liyandeMac-mini.local>
2017-11-16 21:04:14 +0800

    完结框架版

--- webpack-vue-yintai/src/App.vue
+++ webpack-vue-yintai/src/App.vue
@@ -8,9 +8,12 @@
   export default {
     name: 'app',
     mounted () {
+      var date1 = new Date()
+      var date2 = `${date1.getFullYear()}${date1.getMonth()}${date1.getDate()}${date1.getMinutes()}`
+
       this.$request({
         type: 'get',
-        url: 'api?r=201711161814&os=HTML5&client_v=1.0.0&pageid=104001&previewtime=0&methodName=products.template.getpage_1.0.0&method=products.template.getpage&apptype=10&ver=1.0.0&pageindex=1',
+        url: `api?r=${date2}&os=HTML5&client_v=1.0.0&pageid=104001&previewtime=0&methodName=products.template.getpage_1.0.0&method=products.template.getpage&apptype=10&ver=1.0.0&pageindex=1`,
         headers: {},
         params: {},
         success: function (res) {
