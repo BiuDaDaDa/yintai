10c1e33fec321e7ef345a1c712ef01d4107ddde2
dllo <dllo@liyandeMac-mini.local>
2017-11-17 09:37:07 +0800

    post版

--- webpack-vue-yintai/src/App.vue
+++ webpack-vue-yintai/src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
+    <pin-zhi-xb></pin-zhi-xb>
     <router-view/>
   </div>
 </template>
 
 <script>
+  import PinZhiXb from './components/index/PinZhiXB'
   export default {
     name: 'app',
+    components: {
+      PinZhiXb
+    },
     mounted () {
       var date1 = new Date()
-      var date2 = `${date1.getFullYear()}${date1.getMonth()}${date1.getDate()}${date1.getMinutes()}`
-
+      var date2 = `${date1.getFullYear()}${date1.getMonth() + 1}${date1.getDate()}${date1.getMinutes()}`
       this.$request({
         type: 'get',
         url: `api?r=${date2}&os=HTML5&client_v=1.0.0&pageid=104001&previewtime=0&methodName=products.template.getpage_1.0.0&method=products.template.getpage&apptype=10&ver=1.0.0&pageindex=1`,
@@ -28,12 +32,5 @@
 </script>
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
+  @import "common/css/reset.less";
 </style>
---
+++ webpack-vue-yintai/src/components/index/PinZhiXB.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="pinzhi-xb" v-if="thisdata != null">
+    <div class="wrap">
+
+      <div class="header">
+        <div class="header-A">
+          <div class="hA-top">
+            <div class="hA-top-left">
+              <img :src="thisdata['data']['templatelist'][0]['items'][0]['imgurl']" alt="">
+            </div>
+            <div class="hA-top-right">
+              <img :src="thisdata['data']['templatelist'][0]['items'][1]['imgurl']" alt="">
+              <img :src="thisdata['data']['templatelist'][0]['items'][2]['imgurl']" alt="">
+            </div>
+          </div>
+
+          <div class="hA-content">
+            <img :src="thisdata['data']['templatelist'][1]['items'][0]['imgurl']" alt="">
+            <img :src="thisdata['data']['templatelist'][1]['items'][1]['imgurl']" alt="">
+            <img :src="thisdata['data']['templatelist'][1]['items'][2]['imgurl']" alt="">
+          </div>
+
+          <div class="hA-footer">
+            <img :src="thisdata['data']['templatelist'][3]['items'][0]['imgurl']" alt="">
+          </div>
+        </div>
+      </div>
+
+      <div class="content">
+        <div class="content-A">
+          <div class="cA-top">
+            <img :src="thisdata['data']['templatelist'][4]['items'][0]['imgurl']" alt="">
+          </div>
+          <div class="cA-content"></div>
+          <div class="cA-footer"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: '',
+    data () {
+      return {
+        thisdata: null
+      }
+    },
+    mounted () {
+      this.$request({
+        type: 'get',
+        url: 'api?r=201711161835&os=HTML5&client_v=1.0.0&pageid=104001&previewtime=0&methodName=products.template.getpage_1.0.0&method=products.template.getpage&apptype=10&ver=1.0.0&pageindex=2',
+        headers: {},
+        params: {},
+        success: function (res) {
+          this.thisdata = res.data
+          console.log(res.data)
+        },
+        failed: function (err) {
+          console.log(err)
+        }
+      })
+    }
+  }
+</script>
+
+<style scoped lang="less">
+  @import "../../common/css/index";
+  .header-A{
+    width: 100%;
+  }
+  .hA-top{
+    width: 100%;
+    overflow: hidden;
+  }
+  .hA-top-left{
+    width: 50%;
+    float: left;
+  }
+  .hA-top-left img{
+    width: 100%;
+  }
+  .hA-top-right{
+    width: 50%;
+    float: left;
+  }
+  .hA-top-right img{
+    width: 100%;
+    border-bottom: 1px solid rgb(237,237,237);
+    line-height: 0;
+    vertical-align: middle
+  }
+  .hA-content{
+    width: 100%;
+    display: inline-flex;
+  }
+  .hA-content img{
+    width: 33%;
+    height: 100%;
+    object-fit:cover;
+    border-right: 1px solid rgb(237,237,237);
+  }
+  .hA-footer{
+    width: 100%;
+  }
+  .hA-footer img{
+    width: 100%;
+  }
+</style>
