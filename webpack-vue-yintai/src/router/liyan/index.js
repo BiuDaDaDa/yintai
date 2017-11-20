064e203819f9d75367c14bc18b01fb5ac9e01284
dllo <dllo@liyandeMac-mini.local>
2017-11-20 22:07:13 +0800

    feat:#15 by:liyan 完成惠生活页面

--- webpack-vue-yintai/src/router/liyan/index.js
+++ webpack-vue-yintai/src/router/liyan/index.js
@@ -4,6 +4,7 @@ import route from 'vue-router'
 import PinZhiXb from '../../components/index/PinZhiXB'
 import ShangChangTK from '../../components/index/ShangChangTK'
 import VIP from '../../components/index/VIP'
+import Life from '../../components/index/Life'
 
 Vue.component(Swipe.name, Swipe)
 Vue.component(SwipeItem.name, SwipeItem)
@@ -14,21 +15,20 @@ export default [
     path: '/',
     name: 'PinZhiXb',
     component: PinZhiXb
-    // children: [
-    //   {
-    //     path: 'ShangChangTK',
-    //     component: ShangChangTK
-    //   }
-    // ]
   },
   {
-    path: '/ShangChangTK',
+    path: '/21023303',
     name: 'ShangChangTK',
     component: ShangChangTK
   },
   {
-    path: '/VIP',
+    path: '/21023306',
     name: 'VIP',
     component: VIP
+  },
+  {
+    path: '/21023307',
+    name: 'Life',
+    component: Life
   }
 ]
